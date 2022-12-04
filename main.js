fdfe6b333c13b60c036751c1c948b44b1f89f003
Sergei Plotnikov <slavdudeight@gmail.com>
2022-12-04 21:58:14 +0400

    des cipher

--- main.js
+++ main.js
@@ -1,76 +1,88 @@
-const alphabetEng =  'abcdefghigklmnopqrstuvwxyz'.split('')
+// const alphabetEng =  'abcdefghigklmnopqrstuvwxyz'.split('')
 
-// function generateVenegerSquare(alphabet) 
+// // function generateVenegerSquare(alphabet) 
+// // {
+// //     let arr = []
+
+// //     for (let i = 0; i < alphabet.length; i++) 
+// //     {
+// //         arr[i] = alphabet.slice(i).concat(alphabet.slice(0, i))
+// //     }
+
+// //     return arr
+// // }
+
+// function formatKeyword(key, expression) 
 // {
+//     let keyIndexes = getIndexes(key)
+
+//     let iter = 0
+//     let formatedKey = []
+
+//     for (let i = 0; i < expression.length; i++) 
+//     {
+//         formatedKey[i] = keyIndexes[iter]
+//         iter++
+
+//         if (iter >= keyIndexes.length) {
+//             iter = 0
+//         }
+//     }
+
+//     return formatedKey
+// }
+
+
+// function getIndexes(exp) {
 //     let arr = []
 
-//     for (let i = 0; i < alphabet.length; i++) 
-//     {
-//         arr[i] = alphabet.slice(i).concat(alphabet.slice(0, i))
+//     for (let i = 0; i < exp.length; i++) {
+//         arr.push(alphabetEng.indexOf(exp[i]))
 //     }
 
 //     return arr
+// } 
+
+// function encrypt(key, expression) 
+// {
+
+//     let expIndexses = getIndexes(expression)
+//     let formatedKey = formatKeyword(key, expression)
+//     let encryptedWord = ''
+
+//     for (let i = 0; i < expression.length; i++) {
+//         encryptedWord += alphabetEng[(formatedKey[i] + expIndexses[i]) % 26]
+//     } 
+
+//     return encryptedWord
 // }
 
-function formatKeyword(key, expression) 
-{
-    let keyIndexes = getIndexes(key)
+// function decrypt(key, expression)
+// {
+//     let expIndexses = getIndexes(expression)
+//     let formatedKey = formatKeyword(key, expression)
+//     let decryptedWord = ''
 
-    let iter = 0
-    let formatedKey = []
+//     for (let i = 0; i < expression.length; i++) {
+//         let val = (expIndexses[i] - formatedKey[i]) % 26
+//         let index = val > 0 ? val: (val + 26) % 26
+//         decryptedWord += alphabetEng[index]
+//     } 
 
-    for (let i = 0; i < expression.length; i++) 
-    {
-        formatedKey[i] = keyIndexes[iter]
-        iter++
+//     return decryptedWord
+// }
 
-        if (iter >= keyIndexes.length) {
-            iter = 0
-        }
-    }
+// console.log(encrypt('lol', 'imfuckingretarded'))
+// console.log(decrypt('lol', 'taqfqvtbrcselfopr'))
 
-    return formatedKey
-}
+//DES
+const Crypter = require('./des')
 
+const plaintext = 'COMPUTER'
+const key = '13 34 57 79 9B BC DF F1'
 
-function getIndexes(exp) {
-    let arr = []
+const ciphertext = Crypter.Encrypt(plaintext, key)
+const decrypt = Crypter.Decrypt(ciphertext, key)
 
-    for (let i = 0; i < exp.length; i++) {
-        arr.push(alphabetEng.indexOf(exp[i]))
-    }
-
-    return arr
-} 
-
-function encrypt(key, expression) 
-{
-
-    let expIndexses = getIndexes(expression)
-    let formatedKey = formatKeyword(key, expression)
-    let encryptedWord = ''
-
-    for (let i = 0; i < expression.length; i++) {
-        encryptedWord += alphabetEng[(formatedKey[i] + expIndexses[i]) % 26]
-    } 
-
-    return encryptedWord
-}
-
-function decrypt(key, expression)
-{
-    let expIndexses = getIndexes(expression)
-    let formatedKey = formatKeyword(key, expression)
-    let decryptedWord = ''
-
-    for (let i = 0; i < expression.length; i++) {
-        let val = (expIndexses[i] - formatedKey[i]) % 26
-        let index = val > 0 ? val: (val + 26) % 26
-        decryptedWord += alphabetEng[index]
-    } 
-
-    return decryptedWord
-}
-
-console.log(encrypt('lol', 'imfuckingretarded'))
-console.log(decrypt('lol', 'taqfqvtbrcselfopr'))
+console.log(`Ciphertext: ${ciphertext}`)
+console.log(`Decryption: ${decrypt}`)
